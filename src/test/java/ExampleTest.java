1641b43f4fe4daa3d138587e0ad7bb174fa7405a
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2018-11-06 11:30:56 +0100

    Fix test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -11,21 +11,23 @@ import java.io.IOException;
 import java.net.URL;
 
 
-/** Load and validate XML examples against schema. */
-public class Examples {
+/**
+ * Load and validate XML examples against schema.
+ */
+public class ExampleTest {
 
     @Test
     public void testSamples() throws SAXException, IOException {
 
         // Load schema and create validator
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        URL resource = Examples.class.getResource("GenericModel1.0.3.xsd");
+        URL resource = ExampleTest.class.getResource("GenericModel1.0.3.xsd");
         File schemaFile = new File(resource.getFile());
 
         Schema schema = factory.newSchema(schemaFile);
         Validator validator = schema.newValidator();
 
-        File sample0 = new File(Examples.class.getResource("sample0.xml").getFile());
+        File sample0 = new File(ExampleTest.class.getResource("sample0.xml").getFile());
         validator.validate(new StreamSource(sample0));
     }
 }
